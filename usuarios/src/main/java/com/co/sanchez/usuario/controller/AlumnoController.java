3db2fc1c1d49695d9e1da0d301d72d629829b0b6
JuanSO121 <sanchezjuanjose0508@gmail.com>
2024-11-12 18:21:02 -0500

    New

--- usuarios/src/main/java/com/co/sanchez/usuario/controller/AlumnoController.java
+++ usuarios/src/main/java/com/co/sanchez/usuario/controller/AlumnoController.java
@@ -3,10 +3,13 @@ package com.co.sanchez.usuario.controller;
 import com.co.sanchez.usuario.models.entity.Alumno;
 import com.co.sanchez.usuario.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -14,6 +17,9 @@ import java.util.Optional;
 public class AlumnoController {
 
     private final AlumnoService service;
+
+    @Value("${config.balanceador.test}")
+    private String balanceadorTest;
 
     @Autowired
     public AlumnoController(AlumnoService service) {
@@ -25,12 +31,35 @@ public class AlumnoController {
         return ResponseEntity.ok().body(service.findAll());
     }
 
+//    @GetMapping("/balanceador-test")
+//    public ResponseEntity<?> balanceadorTest() {
+//        Map<String, Object> response = new HashMap<String, Object> ();
+//        response.put("balanceador", balanceadorTest);
+//        response.put("alumno", service.findAll());
+//
+//        return ResponseEntity.ok().body(response);
+//    }
+
+    @GetMapping("/balanceador-test")
+    public ResponseEntity<?> balanceadorTest() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            response.put("balanceador", balanceadorTest); // Variable de entorno
+            response.put("alumno", service.findAll()); // Lista de alumnos
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("error", "Error al obtener los datos de alumnos");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<Alumno> getById(@PathVariable Long id) {
         Optional<Alumno> alumno = service.findById(id);
         return alumno.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
 
     @PostMapping
     public ResponseEntity<Alumno> create(@RequestBody Alumno alumno) {
